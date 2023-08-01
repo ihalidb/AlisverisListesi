d5bc315247f99c6d51f51fded9da45775b67110d
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-01 22:22:53 +0300

    klavyeyi dışarı tıkladığımızda kapatmayı yazdık. Galeriye ulaşıp görsel almayı ve alınan görseli editlemeyi başardık.

--- AlisverisListesi/DetailsViewController.swift
+++ AlisverisListesi/DetailsViewController.swift
@@ -7,21 +7,42 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var isimTextField: UITextField!
-    
     @IBOutlet weak var fiyatTextField: UITextField!
-    
     @IBOutlet weak var bedenTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselSec))
+        imageView.addGestureRecognizer(imageGestureRecognizer)
+        
+        //Klavye kapatma-start
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(klavyeyiKapat))
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+    @objc func klavyeyiKapat() {
+        view.endEditing(true)
+    }
+    
+    @objc func gorselSec(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
     }
     
     @IBAction func kaydetButtonTiklandi(_ sender: Any) {
--- AlisverisListesi/ViewController.swift
+++ AlisverisListesi/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(eklemebuttonuTiklandi))
+        
+        
+        
+    }
+    @objc func eklemebuttonuTiklandi (){
+        
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
 
 
