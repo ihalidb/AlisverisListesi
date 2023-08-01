6b900bf83ecd55558705c8e0631dc6013b914fec
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-01 21:39:48 +0300

    Initial Commit

---
+++ AlisverisListesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlisverisListesi
+//
+//  Created by İbrahim Halid Bayrak on 1.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
